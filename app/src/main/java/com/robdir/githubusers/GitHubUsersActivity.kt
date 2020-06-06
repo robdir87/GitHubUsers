3890e44ae8690c88cd3b07bca372f76902235b54
Roberto Di Russo <robertodirusso@gmail.com>
2020-06-06 10:35:34 +0100

    Inits project

---
+++ app/src/main/java/com/robdir/githubusers/GitHubUsersActivity.kt
@@ -0,0 +1,11 @@
+package com.robdir.githubusers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GitHubUsersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_github_users)
+    }
+}
