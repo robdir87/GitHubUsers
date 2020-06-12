220abe0c7882d7444a9f02c87fa3913ae4b33551
Roberto Di Russo <robertodirusso@gmail.com>
2020-06-12 10:51:02 +0100

    Creates UI for user detail and loads basic data

--- app/src/main/java/com/robdir/githubusers/presentation/userdetail/UserDetailActivity.kt
+++ app/src/main/java/com/robdir/githubusers/presentation/userdetail/UserDetailActivity.kt
@@ -7,30 +7,56 @@ import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.robdir.githubusers.GitHubUsersApplication
 import com.robdir.githubusers.databinding.ActivityUserDetailBinding
+import com.squareup.picasso.Picasso
+import javax.inject.Inject
 
 class UserDetailActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var picasso: Picasso
+
     private lateinit var binding: ActivityUserDetailBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         (application as GitHubUsersApplication).appComponent.inject(this)
-        super.onCreate(savedInstanceState, persistentState)
+        super.onCreate(savedInstanceState)
 
         binding = ActivityUserDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupToolbar()
+        displayUserBasicData()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbarUserDetail)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun displayUserBasicData() {
+        intent?.run {
+            binding.toolbarLayoutUserDetail.title = getStringExtra(EXTRA_USERNAME)
+
+            picasso.load(getStringExtra(EXTRA_AVATAR_URL))
+                .noFade()
+                .into(binding.imageViewUserAvatar)
+        }
     }
 
     companion object {
 
-        private const val BUNDLE_KEY_USERNAME = "UserDetailActivity.BUNDLE_KEY_USERNAME"
-        private const val BUNDLE_KEY_AVATAR_URL = "UserDetailActivity.BUNDLE_KEY_AVATAR_URL"
+        private const val EXTRA_USERNAME = "UserDetailActivity.EXTRA_USERNAME"
+        private const val EXTRA_AVATAR_URL = "UserDetailActivity.EXTRA_AVATAR_URL"
 
         fun intent(context: Context, username: String, avatarUrl: String): Intent =
             Intent(context, UserDetailActivity::class.java).apply {
-                putExtra(BUNDLE_KEY_USERNAME, username)
-                putExtra(BUNDLE_KEY_AVATAR_URL, avatarUrl)
+                putExtra(EXTRA_USERNAME, username)
+                putExtra(EXTRA_AVATAR_URL, avatarUrl)
             }
     }
 }
