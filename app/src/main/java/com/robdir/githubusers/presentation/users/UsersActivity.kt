4cd5e434442248e768b75ca5ff1841334721da8f
Roberto Di Russo <robertodirusso@gmail.com>
2020-06-12 08:56:14 +0100

    Creates users adapter and connects it to the users activity

--- app/src/main/java/com/robdir/githubusers/presentation/users/UsersActivity.kt
+++ app/src/main/java/com/robdir/githubusers/presentation/users/UsersActivity.kt
@@ -1,20 +1,27 @@
 package com.robdir.githubusers.presentation.users
 
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.robdir.githubusers.GitHubUsersApplication
-import com.robdir.githubusers.R
+import com.robdir.githubusers.databinding.ActivityUsersBinding
+import com.robdir.githubusers.domain.users.User
 import com.robdir.githubusers.presentation.UsersViewState
 import javax.inject.Inject
 
-class UsersActivity : AppCompatActivity() {
+class UsersActivity : AppCompatActivity(), UsersAdapter.Callback {
 
     @Inject
     lateinit var usersViewModelFactory: UsersViewModelFactory
+
+    @Inject
+    lateinit var usersAdapter: UsersAdapter
+
+    private lateinit var binding: ActivityUsersBinding
 
     private val usersViewModel: UsersViewModel by lazy { viewModel() }
 
@@ -22,29 +29,56 @@ class UsersActivity : AppCompatActivity() {
         (application as GitHubUsersApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_users)
+        binding = ActivityUsersBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        usersViewModel.apply {
-            viewState.observe(
-                this@UsersActivity,
-                Observer { viewState ->
-                    when(viewState) {
-                        is UsersViewState.Loading ->
-                            Toast.makeText(this@UsersActivity, "Loading", Toast.LENGTH_SHORT).show()
-                        is UsersViewState.Loaded -> {
-                            findViewById<TextView>(R.id.textViewUsers).text = viewState.users.toString()
-                        }
-                        is UsersViewState.Error ->
-                            Toast.makeText(this@UsersActivity, "Error", Toast.LENGTH_SHORT).show()
-                        is UsersViewState.NetworkError ->
-                            Toast.makeText(this@UsersActivity, "Network Error", Toast.LENGTH_SHORT).show()
-                    }
+        setupRecyclerViewUsers()
+        setupSearchViewUsers()
+        setupUsersViewModel()
+    }
 
-                }
-            )
+    override fun onUserSelected(user: User) {
+        Toast.makeText(this, "TODO: Selected ${user.username}", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setupRecyclerViewUsers() {
+        usersAdapter.callback = this
+
+        binding.recyclerViewUsers.apply {
+            adapter = usersAdapter
+            layoutManager = LinearLayoutManager(context)
         }
+    }
 
-        findViewById<TextView>(R.id.textViewGo).setOnClickListener { usersViewModel.searchUsers("tom") }
+    private fun setupSearchViewUsers() {
+        binding.searchViewUsers.setOnQueryTextListener(
+            object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(query: String?): Boolean {
+                    usersViewModel.searchUsers(query.orEmpty())
+                    return true
+                }
+
+                override fun onQueryTextSubmit(query: String?): Boolean = false
+            }
+        )
+    }
+
+    private fun setupUsersViewModel() {
+        usersViewModel.viewState.observe(
+            this@UsersActivity,
+            Observer { viewState ->
+                when (viewState) {
+                    is UsersViewState.Loading ->
+                        Toast.makeText(this@UsersActivity, "Loading", Toast.LENGTH_SHORT).show()
+                    is UsersViewState.Loaded ->
+                        usersAdapter.submitList(viewState.users)
+                    is UsersViewState.Error ->
+                        Toast.makeText(this@UsersActivity, "Error", Toast.LENGTH_SHORT).show()
+                    is UsersViewState.NetworkError ->
+                        Toast.makeText(this@UsersActivity, "Network Error", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
     }
 
     private fun viewModel(): UsersViewModel =
---
+++ app/src/main/java/com/robdir/githubusers/presentation/users/UsersAdapter.kt
@@ -0,0 +1,51 @@
+package com.robdir.githubusers.presentation.users
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.robdir.githubusers.databinding.LayoutListItemUserBinding
+import com.robdir.githubusers.domain.users.User
+import com.squareup.picasso.Picasso
+import javax.inject.Inject
+
+class UsersAdapter @Inject constructor(private val picasso: Picasso) :
+    ListAdapter<User, UsersAdapter.ViewHolder>(UserComparator) {
+
+    interface Callback {
+        fun onUserSelected(user: User)
+    }
+    var callback: Callback? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(LayoutListItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        currentList[position]?.let(holder::bind)
+    }
+
+    override fun getItemCount(): Int = currentList.size
+
+    companion object {
+        @JvmStatic
+        private val UserComparator = object : DiffUtil.ItemCallback<User>() {
+
+            override fun areItemsTheSame(oldItem: User, newItem: User): Boolean = oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: User, newItem: User): Boolean = oldItem == newItem
+        }
+    }
+
+    inner class ViewHolder(private val userBinding: LayoutListItemUserBinding) :
+        RecyclerView.ViewHolder(userBinding.root) {
+
+        fun bind(user: User) {
+            with(userBinding) {
+                layoutUserListItem.setOnClickListener { callback?.onUserSelected(user) }
+                textViewUserName.text = user.username
+                picasso.load(user.avatarUrl).into(imageViewThumbnail)
+            }
+        }
+    }
+}
